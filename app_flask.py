5840788353cf5db2e2615e0b05544b79643893ee
Achille SIABI <achille.siabi@gmail.com>
2023-07-12 14:41:08 +0200

    new commit

--- app_flask.py
+++ app_flask.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_restx import Api, Resource, fields
+import os
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://db:27017/mydatabase'  # Connexion à la base de données MongoDB
